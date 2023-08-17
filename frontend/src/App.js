05a194244a528f8782627d963ddf69cf87d75d23
Subhanshu Prasad <subhanshup933@gmail.com>
2023-08-17 18:13:37 +0530

    Listing Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
-
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomeScreen from "./screens/HomeScreen"
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to Shop!</h1>
-    </div>
+    <>
+    <Header />
+    <main className="py-3">
+      <Container>
+        <HomeScreen />
+      </Container>
+      </main> 
+    <Footer />
+    </>
   )
 }
 
