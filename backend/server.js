590b321ab30f2eb945b9fd7d89a56f6aa420fddc
Subhanshu Prasad <subhanshup933@gmail.com>
2023-08-27 23:24:46 +0530

    error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,20 @@
 dotenv.config();
 import express from 'express';
-import products from './data/products.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-
+import productRoutes from './routes/productRoutes.js '
 connectDB(); // connect to database
 const port = process.env.PORT || 8000; 
 
 const app = express();
 
-app.get('/', (req,res)=>{
+app.post('/', (req,res)=>{
     res.send("API is running..");
 })
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find((p)=>p._id === req.params.id)
-    res.json(product);
-})
-
+app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`);
 })
